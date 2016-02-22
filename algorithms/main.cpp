01b8122401c96e0b6a2150c17589fcc9fe9c3ea6
Evgeniy Shumakov <evgeny@async.local>
2016-02-22 11:57:22 +0300

    Initial Commit

---
+++ algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by Evgeniy Shumakov on 22.02.16.
+//  Copyright © 2016 Evgeniy Shumakov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
